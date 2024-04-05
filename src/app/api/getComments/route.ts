ef279290d8bd791dae81179ca2727a4d4e7c50aa
Mohamed Jalal <technochtak3@gmail.com>
2024-04-05 21:27:14 +0000

    main

--- src/app/api/getComments/route.ts
+++ src/app/api/getComments/route.ts
@@ -1,8 +1,6 @@
 import { NextRequest } from "next/server";
 
-const handler = (req: NextRequest) => {
+export default async function GET(req: NextRequest) {
   const apiUrl =
     "https://graph.facebook.com/v19.0/me/accounts?access_token=536271d37a39a422138dac4c6b92c309";
-};
-
-export default handler;
+}
