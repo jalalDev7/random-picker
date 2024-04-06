429a744d8a97656140e6a80658e197e62e905c30
Mohamed Jalal <technochtak3@gmail.com>
2024-04-06 00:32:22 +0000

    main

--- src/app/api/getComments/route.ts
+++ src/app/api/getComments/route.ts
@@ -1,7 +1,10 @@
 import { NextRequest } from "next/server";
 
-export async function GET(req: NextRequest) {
+export async function GET() {
   const apiUrl =
     "https://graph.facebook.com/v19.0/me/accounts?access_token=536271d37a39a422138dac4c6b92c309";
-  return console.log("done");
+  const res = await fetch(apiUrl);
+  const data = await res.json();
+
+  return Response.json({ data });
 }
